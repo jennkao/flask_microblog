99d5375d14e11f93fb177feceb45bfbd623a4cbc
Jennifer Kao <jennkao92@gmail.com>
2017-10-21 17:17:16 -0700

    Create Login form

---
+++ config.py
@@ -0,0 +1,9 @@
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'decision-anxiety-when-ordering-food'
+
+OPENID_PROVIDERS = [
+  {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+  {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+  {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+  {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
+]
