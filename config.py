a3a6243c25f03f5f515284d955921cb7540133be
Jennifer Kao <jennkao92@gmail.com>
2017-10-22 22:53:18 -0700

    Add models and db_create script

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'decision-anxiety-when-ordering-food'
 
@@ -7,3 +8,8 @@ OPENID_PROVIDERS = [
   {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
   {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
